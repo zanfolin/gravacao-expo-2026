7166d98582244803146f000ccd92649d52c14901
Graziani Zanfolin <zanfolin@gmail.com>
2026-08-23 21:38:08 -0300

    [feat] Gluestack

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native';
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+import { Button, ButtonText } from './components/ui/button';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Gravação Aula</Text>
-      <Text style={styles.negrito}>DDM I - 2026</Text>
-    </View>
+
+    <GluestackUIProvider mode="dark">
+      <View style={styles.container}>
+        <Text>Gravação Aula</Text>
+        <Text style={styles.negrito}>DDM I - 2026</Text>
+        <Button variant="secondary" size="default">
+          <ButtonText className=' font-bold uppercase'>2o. INFO - etec</ButtonText>
+        </Button>
+      </View>
+    </GluestackUIProvider>
+
   );
 }
 
