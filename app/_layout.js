6f5a47bd13b70d8f5d64cd7429410b79458b93b2
Graziani Zanfolin <zanfolin@gmail.com>
2026-09-05 09:49:32 -0300

    feat: enhance Home component layout and add Divider and Heading components

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,6 @@
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { Stack } from "expo-router";
+import "@/global.css"
 
 export default function RootLayout() {
     return (
--- app/home.jsx
+++ app/home.jsx
@@ -1,11 +1,28 @@
+import { Box } from "@/components/ui/box";
+import { Button, ButtonText } from "@/components/ui/button";
+import { Divider } from "@/components/ui/divider";
+import { Heading } from "@/components/ui/heading";
 import { Text } from "@/components/ui/text";
+import { VStack } from "@/components/ui/vstack";
 import { Link } from "expo-router";
 
 export default function Home() {
     return (
-        <>
-            <Text>Página Principal</Text>
-            <Link href="/details">Detalhhes</Link>
-        </>
+        <Box className="flex-1 bg-white p-6">
+            <VStack space="lg">
+                <Heading size="xl">Tela Inicial</Heading>
+                <Text size="md">
+                    Esta é a rota "/" (arquivo app/home.jsx). Os botões abaixo levam para a mesma tela de detalhes,
+                    usaando as formas de navegar do expo-router.
+                </Text>
+                <Divider />
+                <Link href="/details" asChild>
+                    <Button size="lg" action="primary">
+                        <ButtonText>Ir com o Link (declarativo)</ButtonText>
+                    </Button>
+                </Link>
+
+            </VStack>
+        </Box>
     )
 }
