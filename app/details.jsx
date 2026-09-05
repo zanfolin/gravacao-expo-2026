63753546e596754e78c824d85f4e6af29f864423
Graziani Zanfolin <zanfolin@gmail.com>
2026-09-05 10:16:37 -0300

    feat: implement Center component and enhance Details and Home layouts with new UI elements

--- app/details.jsx
+++ app/details.jsx
@@ -1,11 +1,30 @@
+import { Box } from "@/components/ui/box";
+import { Button, ButtonText } from "@/components/ui/button";
+import { Divider } from "@/components/ui/divider";
 import { Text } from "@/components/ui/text";
-import { Link } from "expo-router";
+import { VStack } from "@/components/ui/vstack";
+import { Link, useLocalSearchParams, useRouter } from "expo-router";
 
 export default function Details() {
+    const router = useRouter();
+    const { name } = useLocalSearchParams();
     return (
-        <>
-            <Text>Detalhes</Text>
-            <Link href="/home">Página Principal</Link>
-        </>
+        <Box className="flex-1 bg-white p-6">
+            <VStack space="lg">
+                <Text size="xl">
+                    Esta é a rota "/details" (arquivo app/details.jsx)
+                </Text>
+                <Divider />
+                <Text size="md">
+                    Parâmetro recebido da outra tela: <Text className="font-extrabold text-amber-700">{name ? name : "Sem Parâmetro"}</Text>
+                </Text>
+                <Button size="lg" action="primary" onPress={() => router.back()}
+                    className="bg-emerald-600 data-[hover=true]:bg-emerald-500 data-[active=true]:bg-emerald-700  "
+                    >
+                    <ButtonText className="font-bold uppercase">Voltar</ButtonText>
+                </Button>
+
+            </VStack>
+        </Box>
     )
 }
--- app/home.jsx
+++ app/home.jsx
@@ -1,12 +1,16 @@
 import { Box } from "@/components/ui/box";
 import { Button, ButtonText } from "@/components/ui/button";
+import { Center } from "@/components/ui/center";
 import { Divider } from "@/components/ui/divider";
 import { Heading } from "@/components/ui/heading";
 import { Text } from "@/components/ui/text";
 import { VStack } from "@/components/ui/vstack";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 
 export default function Home() {
+    const router = useRouter();
+
+
     return (
         <Box className="flex-1 bg-white p-6">
             <VStack space="lg">
@@ -21,6 +25,29 @@ export default function Home() {
                         <ButtonText>Ir com o Link (declarativo)</ButtonText>
                     </Button>
                 </Link>
+                <Divider />
+                <Button
+                    size="lg"
+                    action="secodary"
+                    onPress={() => router.push("/details")}
+                >
+                    <ButtonText>Ir com router.push</ButtonText>
+                </Button>
+                <Center>
+                    <Divider className="my-0.5 w-20 bg-emerald-500" />
+                </Center>
+                <Button
+                    size="lg"
+                    variant="outline"
+                    onPress={() => {
+                        router.push({
+                            pathname: "/details",
+                            params: { name: "2o. INFO" }
+                        })
+                    }}
+                >
+                    <ButtonText>Ir enviando um parâmetro</ButtonText>
+                </Button>
 
             </VStack>
         </Box>
